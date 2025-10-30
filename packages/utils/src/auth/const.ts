b8b572806c0d5e824c9c8bd06f7513d514b40054
Abhishek Kolge <abhishek.kolge@soar.inc>
2025-10-31 02:52:43 +0530

    fix: update default credits from 10 to 50

--- packages/utils/src/auth/const.ts
+++ packages/utils/src/auth/const.ts
@@ -1,4 +1,4 @@
 export const SESSION_EXPIRES_IN = 86_400;
 export const SESSION_UPDATE_AGE = 28_800;
-export const DEFAULT_CREDITS = 10;
+export const DEFAULT_CREDITS = 50;
 export const CREDIT_RESET_HOUR = 6;
